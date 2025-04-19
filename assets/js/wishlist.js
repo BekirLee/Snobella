8c2883c5082859685deef4a62ea4c12b49de77ae
BekirLee <nihadbekirli8@gmail.com>
2025-04-19 14:16:49 +0400

    changes

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,8 +1,7 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 const wishlistIDs = currentUser?.wishlist || [];
 
-wishlistIDs.forEach(async id => {
-    const res = await fetch(`${BASE_URL}/products/${id}`);
-    const product = await res.json();
-
+wishlistIDs.forEach(async (id) => {
+  const res = await fetch(`${BASE_URL}/products/${id}`);
+  const product = await res.json();
 });
